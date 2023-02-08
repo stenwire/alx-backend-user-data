15efb8db4f677fd54de48198642c1cf45a79f1d4
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-02-08 20:50:19 +0100

    add-401-route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+  """ GET /api/v1/unauthorized
+    Return:
+      - a 401 error response
+    """
+  abort(401)
