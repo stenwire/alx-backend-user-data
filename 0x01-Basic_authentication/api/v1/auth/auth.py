83089f16125931349f817726136f51ae822a5a67
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-02-08 23:21:06 +0100

    chore: update-require_auth-function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,9 +2,23 @@
 """
 Route module to manage the API authentication
 """
+import re
 from flask import request
 from typing import List
 from typing import TypeVar
+
+
+def format_path(path: List[str]) -> List:
+    """formats path string
+    """
+    if isinstance(path, list):
+        p = ''.join(str(x) for x in path)
+        result = re.split('|/|', p)
+    else:
+        result = re.split('|/|', path)
+    while ("" in result):
+        result.remove("")
+    return result
 
 
 class Auth():
@@ -14,7 +28,14 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """secures a route
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if format_path(path) == format_path(excluded_paths):
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """adds authorization header to request body
